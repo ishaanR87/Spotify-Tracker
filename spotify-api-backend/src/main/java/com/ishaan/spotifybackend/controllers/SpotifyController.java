ae497f99898a6f889888db4407b00d07f54479f3
Ishaan <i.rawat98@gmail.com>
2023-12-12 18:22:54 +0000

    authorisation flow redirecting working (testing on postman)

--- spotify-api-backend/src/main/java/com/ishaan/spotifybackend/controllers/SpotifyController.java
+++ spotify-api-backend/src/main/java/com/ishaan/spotifybackend/controllers/SpotifyController.java
@@ -4,16 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
 import java.net.URI;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
@@ -23,25 +22,44 @@ public class SpotifyController {
  
     private static final String clientId = "f97f541285d24c52a12939a59d65a69c";
     private static final String clientSecret = "f186b4e66470458e9ba2f50ca40042bb";
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/callback");
+   private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/callback");
     
-    private String code = "";
-
-    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-    .setClientId(clientId)
-    .setClientSecret(clientSecret)
-    .setRedirectUri(redirectUri)
-    .build();
-
-    @GetMapping("login")
-    @ResponseBody
+    private static SpotifyApi spotifyApi;
+    
+    // Endpoint to initiate the Spotify authorization process
+    @GetMapping("/login")
     public String spotifyLogin() {
-     AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-//          .state("x4xkmn9pu3j6ukrs8n")
-//          .scope("user-read-birthdate,user-read-email")
-//          .show_dialog(true)
-    .build();
+        spotifyApi = new SpotifyApi.Builder()
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .setRedirectUri(redirectUri)
+                .build();
+        
+       
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+                .scope("user-read-private,user-read-email") 
+                .show_dialog(true) 
+                .build();
+        
+        URI uri = authorizationCodeUriRequest.execute();
+        return uri.toString(); 
     }
     
 
+    @GetMapping("/callback")
+    public String spotifyCallback(@RequestParam("code") String code) {
+        try {
+            AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
+                    .build();
+    
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            
+            return "Authorization Successful!"; 
+        } catch (Exception e) {
+            return "Authorization Failed: " + e.getMessage(); 
+        }
+    }
 }
