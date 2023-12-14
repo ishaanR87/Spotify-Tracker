a1bbebf35479c53ec6a6081260d45ec34425f5e4
Ishaan <i.rawat98@gmail.com>
2023-12-14 01:03:37 +0000

    working on frontend for navbar page which will allow user to switch through different pages

--- spotify-api-frontend/App.jsx
+++ spotify-api-frontend/App.jsx
@@ -1,12 +1,14 @@
 import Login from "./src/components/Login";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./src/components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
--- spotify-api-frontend/src/components/Navbar.jsx
+++ spotify-api-frontend/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <nav className="nav">
-      <a href="/home" className="home"></a>
+      <a href="/home" className="home">
+        Spotify Tracker
+      </a>
       <ul>
-        <li>My Top Artists</li>
-        <li>My Top Songs</li>
-        <li>My Playlists</li>
+        <li>
+          <a href="/top-artists">My Top Artists</a>
+        </li>
+        <li>
+          <a href="/top-songs">My Top Songs</a>
+        </li>
+        <li>
+          <a href="/my-playlist">My Playlist</a>
+        </li>
       </ul>
     </nav>
   );
