446d65a04a7eb58cadff62b7e885aec269af344f
Ishaan <i.rawat98@gmail.com>
2024-04-03 11:25:37 +0100

    removed build options

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "src/main.jsx",
-      },
-    },
   },
 });
